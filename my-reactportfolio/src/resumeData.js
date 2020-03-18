e68788d062a78085b96957492b0c67e119dd808c
David Riveraperez <david.i.riveraperez@gmail.com>
2020-03-17 21:53:03 -0500

    Adding togithub already in heroku

---
+++ my-reactportfolio/src/resumeData.js
@@ -0,0 +1,109 @@
+let resumeData = {
+    "imgurl":"images/Profilepicture.jpg",
+    "name": "David Ivan Riveraperez",
+    "role": "Full Stack Web Developer Student and a Relationship Banker",
+    "linkedinId":"b6a1012a",
+
+    "roleDescription":" I am a currently seeking for opportunities to transition into the field as an Independent Contractor, Part-Time or Full-Time employee. ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/david-ivan-rivera-perez-b6a1012a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/david197116",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"GMail",
+          "url":"https://david.i.riveraperez@gmail.com",
+          "className":"fa fa-envelope"
+        }
+
+      ],
+    "aboutme":"I am 49 yrs old from Puerto Rico, married with two adult children. I retired from the military nine years ago after serving in the US Army over 20 years. I have been working in finance since then and looking for a new career change. Family is very important to me and the motivation that allows me to keep going.",
+    "address":"Round Rock, Texas, (512)695-2652",
+    "email": "david.i.riveraperez@gmail.com ",
+    "education":[
+      {
+        "UniversityName":"The University of Texas at Austin",
+        "specialization":"Full Stack Web Developer",
+        "MonthOfPassing":"Attending",
+        "YearOfPassing":""
+
+      },
+      {
+        "UniversityName":"Grantham University",
+        "specialization":"BS Engineering Management",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2010",
+        "Achievements":"Cum Laude"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"JPMorgan Chase Bank",
+        "specialization":"Relationship and Small Business Banker",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":""
+      },
+      {
+        "CompanyName":"US Army",
+        "specialization":"Combat Operations, Instructor",
+        "MonthOfLeaving":"Jun",
+        "YearOfLeaving":"2011"
+      }
+    ],
+
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"React.js"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"MySQL"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"What2Eat",
+        "description":" Wat2Eat",
+        "imgurl":"images/Wat2Eat project.png",
+        "url": "https://david197116.github.io/Class-Project-1/"
+      },
+      {
+        "name":"areYou Smart",
+        "description":"areYou Smart ",
+        "imgurl":"images/areYou Smart.png",
+        "url":"https://glacial-oasis-54478.herokuapp.com/"
+      },
+      {
+        "name":"Developer Profile Generator",
+        "description":"Developer Profile Generator",  
+        "imgurl":"images/profilegenerator.png",
+        "url":" https://drive.google.com/file/d/1ZcFW8uqh-DfvetXUAjzLl_FOMy2S67Qr/view"
+
+      },
+      {
+        "name":"Devour that Burger ",
+        "description":"Devour that Burger ",
+        "imgurl":"images/Devour  the Burger.png",
+        "url": " https://appsuperburger.herokuapp.com/"
+      }
+    ]
+  }
+  
+  export default resumeData
